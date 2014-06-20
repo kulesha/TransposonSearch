9a227cdd149da87696d7aa243f56ee557c6d4693
Eugene Kulesha <kulesha@gmail.com>
2014-06-20 16:32:10 +0100

    typo ?

--- index.php
+++ index.php
@@ -10,13 +10,6 @@ Choose a file in CSV format: <input type="file" name="src" /> <br/>
 Max distance : <input type="text" name="mind" value="1000"/> <br/>
 <input type="submit" name="search" value="Find"/>
 </form>
-<?php
-MAX_DISTANCE = 1000;
 
-if (array_key_exists("search", $_GET)) {
-   echo "Performing search ... ";
-
-}
-?>
  </body>
 </html>		
